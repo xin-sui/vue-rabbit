cc7169c25997d9cfc182fba5ec90a1d34d9d893c
xin-sui <2505258205@qq.com>
2023-12-05 13:55:00 +0800

    完成主题色定制

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,14 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <el-row class="mb-4">
+      <el-button disabled>Default</el-button>
+      <el-button type="primary" disabled>Primary</el-button>
+      <el-button type="success" disabled>Success</el-button>
+      <el-button type="info" disabled>Info</el-button>
+      <el-button type="warning" disabled>Warning</el-button>
+      <el-button type="danger" disabled>Danger</el-button>
+    </el-row>
   </header>
 
   <RouterView />
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()]
+        }),
+        Components({
+            resolvers: [
+                //1.配置element采用sass样式配色系统
+                ElementPlusResolver({
+                    importStyle: "sass"
+                    // directives: true,
+                    // version: "2.1.5",
+                })
+            ]
+        })
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                //2.自动导入定制化样式覆盖
+                additionalData: `@use "@/styles/element/index.scss" as *;`
+            }
+        }
     }
-  }
-})
+});

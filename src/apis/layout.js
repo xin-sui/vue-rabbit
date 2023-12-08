0837ee97b42c29888251aa0ce02b91f810ea1a38
xin-sui <2505258205@qq.com>
2023-12-08 15:50:49 +0800

    layout-渲染一级导航列表

---
+++ src/apis/layout.js
@@ -0,0 +1,7 @@
+import httpInstance from "@/utils/https";
+// 定义一个函数getCategoryAPI，用于获取分类信息
+export  function getCategoryAPI() {
+    return  httpInstance({
+        url:'home/category/head'
+    })
+}

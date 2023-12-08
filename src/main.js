868bcd4d8b6b9445d2f3328451943427c50b5a9b
xin-sui <2505258205@qq.com>
2023-12-08 16:40:49 +0800

    吸顶导航交互实现

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ import App from "./App.vue";
 import router from "./router";
 //初始化样式
 import '@/styles/common.scss'
-//测试接口函数
-import {getCategoryList} from "@/apis/testApi";
-getCategoryList().then((res) => {
-    console.log(res);
-});
+// //测试接口函数
+// import {getCategoryList} from "@/apis/testApi";
+// getCategoryList().then((res) => {
+//     console.log(res);
+// });
 const app = createApp(App);
 
 app.use(createPinia());
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -4,11 +4,12 @@ import { RouterView } from 'vue-router';
 import LayoutHeader from "@/views/Layout/components/LayoutHeader.vue";
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutFooter from './components/LayoutFooter.vue';
-
+import LayoutFixed from "@/views/Layout/components/LayoutFixed.vue";
 </script>
 
 <template>
     <div>
+      <LayoutFixed></LayoutFixed>
         <LayoutHeader></LayoutHeader>
         <LayoutNav></LayoutNav>
         <!-- 路由出口 -->

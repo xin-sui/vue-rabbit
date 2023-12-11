7c40e41019f38c749e2a44453a3f5b51d6bba65f
xin-sui <2505258205@qq.com>
2023-12-11 11:32:03 +0800

    整体架构拆分以及分类梳理

---
+++ src/views/Home/components/HomeNew.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  新鲜好物
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,21 @@
-<template>
-    <div>
-        HOME
-    </div>
-</template>
-
 <script setup>
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+
 
 </script>
+
+<template>
+  <div class="container">
+    <HomeCategory></HomeCategory>
+    <HomeBanner></HomeBanner>
+  </div>
+  <HomeNew></HomeNew>
+  <HomeHot></HomeHot>
+  <HomeProduct></HomeProduct>
+</template>
 
 <style lang="scss" scoped></style>

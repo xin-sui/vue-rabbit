7bb9f3ec6f766575f5b3073caf93e694ecde9e91
xin-sui <2505258205@qq.com>
2023-12-11 16:50:45 +0800

    home面板组件封装

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -1,9 +1,15 @@
 <script setup>
+import HomePanel from "./HomePanel.vue";
+
 
 </script>
 
 <template>
-  新鲜好物
+  <HomePanel :title="'123'" :sub-title="'你好'">
+    <template #main>
+      123
+    </template>
+  </HomePanel>
 </template>
 
 <style scoped lang="scss">

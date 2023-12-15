7fa829ed9f080290ba3f06eb883ae8a8d4d83ad4
xin-sui <2505258205@qq.com>
2023-12-15 16:00:00 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -4,6 +4,8 @@ import { useRoute } from "vue-router"
 import { getCategoryFilterAPI, getSubCategoryAPI } from '@/apis/category';
 import GoodsItem from '../Home/components/GoodsItem.vue';
 
+
+
 const route = useRoute();
 //二级分类面包屑
 const categoryData = ref([])
@@ -14,7 +16,7 @@ const getCategoryFilter = async () => {
     categoryData.value = res.result
 }
 //二级分类商品数据列表
-const goodsList = ref({})
+const goodsList = ref([])
 const reqData = ref({
     categoryId: route.params.id,
     page: 1,
@@ -22,8 +24,27 @@ const reqData = ref({
     sortField: 'publishTime' //| 'orderNum' | 'evaluateNum'
 })
 const getGoodsList = async () => {
-    const res = await getSubCategoryAPI(reqData)
-    goodsList.value = res.result
+    const res = await getSubCategoryAPI(reqData.value)
+    goodsList.value = res.result.items
+}
+//切换tap时重新请求数据
+const tapChange = async () => {
+    console.log(reqData.value.sortField);
+    // 设置当前页码为1
+    reqData.value.page = 1
+    // 调用获取商品列表函数
+    await getGoodsList()
+}
+const disabled = ref(false)
+//滚到底部加载数据
+const load = async () => {
+    // 获取下一页数据
+    reqData.value.page++
+    const res = await getSubCategoryAPI(reqData.value)
+    goodsList.value = [...goodsList.value, ...res.result.items]
+    if (res.result.items.length == 0) {
+        disabled.value = true
+    }
 }
 onMounted(() => {
     // 当组件加载完毕时调用
@@ -44,14 +65,14 @@ onMounted(() => {
             </el-breadcrumb>
         </div>
         <div class="sub-container">
-            <el-tabs>
+            <el-tabs v-model="reqData.sortField" @tab-change="tapChange">
                 <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
                 <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
                 <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
             </el-tabs>
-            <div class="body">
+            <div v-infinite-scroll="load" :infinite-scroll-disabled="disabled" class="body">
                 <!-- 商品列表-->
-                <GoodsItem v-for="goods in goodsList.items" :goods="goods" :key="goods.id"></GoodsItem>
+                <GoodsItem v-for="goods in goodsList" :goods="goods" :key="goods.id"></GoodsItem>
             </div>
         </div>
     </div>
